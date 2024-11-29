8232694baa5d092c9531e92910afdd3220c92b57
Aaliyah Farah Adibah <73101444+aaliyahfarah@users.noreply.github.com>
2024-11-29 16:48:13 +0700

    Update Square.java

--- day2/Square.java
+++ day2/Square.java
@@ -11,21 +11,23 @@ public class Square extends Shape {
 
     public Float side;
 
-    // public Float process(Float side) {
-    //     return side * side;
-    // }
+    public Float process(String type, Float side) {
+        if(type.equalsIgnoreCase("area")){
+            return side*side; 
+        }
+        else if(type.equalsIgnoreCase("around")){
+            return 4 * side;
+        }
+        else{
+            return side * side * side;
+        }
+    }
 
 
     public void result(String name, String type){
         super.result(name, type);
         if(type.equalsIgnoreCase("area")){
-            System.out.println(side*side); 
-        }
-        else if(type.equalsIgnoreCase("around")){
-            System.out.println(4 * side);
-        }
-        else{
-            System.out.println(side * side * side);
+            System.out.println(process(type, side));
         }
     }
 }
